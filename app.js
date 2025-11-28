e295b1b7b50ba6e262644040035fd8a6b043c454
Apander <Apander204@gmail.com>
2025-11-28 22:01:04 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config();
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT;
+
+const index = require('./routers/index');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'))
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", index);
+
+app.locals.messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+app.listen(PORT || 3000, () => {
+    console.log("Success");
+});
